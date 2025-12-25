6faf81442f9743191382bb3fdc704c93bb214eab
JamesZhang2 <jameszhang2019@outlook.com>
2025-12-25 14:33:41 -0800

    Finish Part 2 of Day 6
    I'll take a short break.

--- src/Day6.java
+++ src/Day6.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Day6 {
     public static void main(String[] args) {
+//        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    private static long part1() {
         List<List<Integer>> nums = new ArrayList<>();
         List<Character> ops = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,10 +38,10 @@ public class Day6 {
             throw new RuntimeException(e);
         }
 
-        System.out.println(part1(nums, ops));
+        return part1Helper(nums, ops);
     }
 
-    private static long part1(List<List<Integer>> nums, List<Character> ops) {
+    private static long part1Helper(List<List<Integer>> nums, List<Character> ops) {
         long ans = 0;
         for (int i = 0; i < nums.getFirst().size(); i++) {
             if (ops.get(i) == '+') {
@@ -51,6 +56,76 @@ public class Day6 {
                     prod *= row.get(i);
                 }
                 ans += prod;
+            }
+        }
+        return ans;
+    }
+
+    private static long part2() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<char[]> nums = new ArrayList<>();
+        char[] ops = null;
+        // The number of lines of numbers is variable - it's not always 3.
+        try {
+            String line = br.readLine();  // don't strip
+            while (!line.isEmpty()) {
+                char[] chars = line.toCharArray();
+                if (chars[0] == '*' || chars[0] == '+') {
+                    // operations
+                    ops = chars;
+                } else {
+                    // numbers
+                    nums.add(chars);
+                }
+                line = br.readLine();
+            }
+            assert ops.length == nums.getFirst().length;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        long ans = 0;
+        int idx = 0;
+        while (idx < ops.length) {
+            if (ops[idx] == '+') {
+                long sum = 0;
+                boolean allEmpty = false;
+                while (!allEmpty && idx < ops.length) {
+                    allEmpty = true;
+                    long num = 0;
+                    for (char[] row : nums) {
+                        if (row[idx] != ' ') {
+                            num = num * 10 + (row[idx] - '0');
+                            allEmpty = false;
+                        }
+                    }
+                    if (!allEmpty) {
+                        sum += num;
+                    }
+                    idx++;
+                }
+                ans += sum;
+            } else if (ops[idx] == '*') {
+                long prod = 1;
+                boolean allEmpty = false;
+                while (!allEmpty && idx < ops.length) {
+                    allEmpty = true;
+                    long num = 0;
+                    for (char[] row : nums) {
+                        if (row[idx] != ' ') {
+                            num = num * 10 + (row[idx] - '0');
+                            allEmpty = false;
+                        }
+                    }
+                    if (!allEmpty) {
+                        prod *= num;
+                    }
+                    idx++;
+                }
+                ans += prod;
+            } else {
+                // impossible
+                assert false;
             }
         }
         return ans;
