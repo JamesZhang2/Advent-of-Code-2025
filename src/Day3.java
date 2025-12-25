709a898a7c2010f7cb9c24dee70f1129a5a52781
JamesZhang2 <jameszhang2019@outlook.com>
2025-12-25 12:15:11 -0800

    Finish Part 2 of Day 3
    Out for lunch

--- src/Day3.java
+++ src/Day3.java
@@ -26,18 +26,19 @@ public class Day3 {
             }
         }
 
-        System.out.println(part1(batteries));
+//        System.out.println(part1(batteries));
+        System.out.println(part2(batteries));
     }
 
     private static int part1(int[][] batteries) {
         int ans = 0;
         for (int[] bank : batteries) {
-            ans += getMaxJoltage(bank);
+            ans += getMaxJoltagePart1(bank);
         }
         return ans;
     }
 
-    private static int getMaxJoltage(int[] bank) {
+    private static int getMaxJoltagePart1(int[] bank) {
         // Just a simple double loop would do
         int max = -1;
         for (int i = 0; i < bank.length; i++) {
@@ -46,5 +47,41 @@ public class Day3 {
             }
         }
         return max;
+    }
+
+    private static long part2(int[][] batteries) {
+        long ans = 0;
+        for (int[] bank : batteries) {
+            ans += getMaxJoltagePart2(bank);
+        }
+        return ans;
+    }
+
+    private static long getMaxJoltagePart2(int[] bank) {
+        // For 12 batteries we definitely need DP.
+        // dp[i][j] is max joltage starting from i, with j more to choose.
+        // There must be at least j batteries left.
+        long[][] dp = new long[bank.length][13];
+
+        for (int i = bank.length - 1; i >= 0; i--) {
+            for (int j = 1; j <= Math.min(12, bank.length - i); j++) {
+                if (i == bank.length - 1) {
+                    // base case
+                    dp[i][j] = bank[i];
+                    continue;
+                }
+
+                if (j == bank.length - i) {
+                    // base case, must choose all
+                    dp[i][j] = bank[i] * (long)Math.pow(10, j - 1) + dp[i + 1][j - 1];
+                    continue;
+                }
+
+                // inductive case: can choose whether to take next or not
+                dp[i][j] = Math.max(bank[i] * (long)Math.pow(10, j - 1) + dp[i + 1][j - 1], dp[i + 1][j]);
+            }
+        }
+
+        return dp[0][12];
     }
 }
