3fc34df22b2c96de2450b222401c1bb10a2972fc
JamesZhang2 <jameszhang2019@outlook.com>
2025-12-25 21:47:42 -0800

    Finish Part 2 of Day 11
    Resumed at 9:30 pm

--- src/Day11.java
+++ src/Day11.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Day11 {
-    private static final Map<String, Long> dp = new HashMap<>();  // Maps node name to number of paths to out
+    // The string s maps to the following map: node name v maps to number of paths from v to s.
+    // For example, dp.get("out") is a map that maps v to number of paths from v to "out".
+    private static final Map<String, Map<String, Long>> dp = new HashMap<>();
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,26 +31,55 @@ public class Day11 {
             throw new RuntimeException(e);
         }
 
-        System.out.println(part1(graph));
+//        System.out.println(part1(graph));
+        System.out.println(part2(graph));
     }
 
     private static long part1(Map<String, List<String>> graph) {
         // Hopefully there aren't any cycles on a path from you to out, otherwise the answer would be infinity.
         // DP on graph
         // Could do bottom-up with topological sort. I think top-down is easier.
-        dp.put("out", 1L);
-        return pathsToOut(graph, "you");
+        return pathsToTarget(graph, "you", "out");
     }
 
-    private static long pathsToOut(Map<String, List<String>> graph, String src) {
-        if (dp.containsKey(src)) {
-            return dp.get(src);
+    private static long part2(Map<String, List<String>> graph) {
+        // either we can reach fft from dac or dac from fft, but not both, because that would imply a cycle.
+        if (pathsToTarget(graph, "dac", "fft") != 0) {
+//            System.out.println("Can go from dac to fft");
+            return pathsToTarget(graph, "svr", "dac")
+                    * pathsToTarget(graph, "dac", "fft")
+                    * pathsToTarget(graph, "fft", "out");
+        } else {
+            assert pathsToTarget(graph, "fft", "dac") != 0;
+//            System.out.println("Can go from fft to dac");
+            return pathsToTarget(graph, "svr", "fft")
+                    * pathsToTarget(graph, "fft", "dac")
+                    * pathsToTarget(graph, "dac", "out");
+        }
+    }
+
+    private static long pathsToTarget(Map<String, List<String>> graph, String src, String target) {
+        if (!dp.containsKey(target)) {
+            dp.put(target, new HashMap<>());
+        }
+        if (dp.get(target).containsKey(src)) {
+            return dp.get(target).get(src);
+        }
+        if (src.equals(target)) {
+            // Assuming no cycles
+            dp.get(target).put(target, 1L);
+            return 1L;
+        }
+        if (!graph.containsKey(src)) {
+            // reached a node with outdegree 0
+            dp.get(target).put(src, 0L);
+            return 0L;
         }
         long ans = 0;
         for (String neigh : graph.get(src)) {
-            ans += pathsToOut(graph, neigh);
+            ans += pathsToTarget(graph, neigh, target);
         }
-        dp.put(src, ans);
+        dp.get(target).put(src, ans);
         return ans;
     }
 }
