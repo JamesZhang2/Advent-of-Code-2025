1d57429ca1b9272e9004d5719f2270b1058d36a4
JamesZhang2 <jameszhang2019@outlook.com>
2025-12-25 22:26:35 -0800

    Add exploration code for Part 2 of Day 9

--- src/Day9.java
+++ src/Day9.java
@@ -21,7 +21,9 @@ public class Day9 {
             throw new RuntimeException(e);
         }
 
-        System.out.println(part1(points));
+//        System.out.println(part1(points));
+        System.out.println("Clockwise? " + clockwise(points));  // My input is counterclockwise
+        System.out.println("Has size-1 steps? " + hasSizeOneSteps(points));  // My input does not have size-1 steps
     }
 
     private static long part1(List<int[]> points) {
@@ -37,5 +39,78 @@ public class Day9 {
 
     private static long getRectSize(int[] p1, int[] p2) {
         return (long)(Math.abs(p1[0] - p2[0]) + 1) * (Math.abs(p1[1] - p2[1]) + 1);
+    }
+
+    // Returns true if the points enclose a region clockwise, false if they enclose a region counterclockwise.
+    private static boolean clockwise(List<int[]> points) {
+        int deg = 0;  // clockwise is positive
+        char dir = dir(points.get(0), points.get(1));
+        for (int i = 1; i <= points.size(); i++) {
+            char newDir = dir(points.get(i % points.size()), points.get((i + 1) % points.size()));
+            switch (dir) {
+                case 'U':
+                    if (newDir == 'L') {
+                        deg -= 90;
+                    } else if (newDir == 'R') {
+                        deg += 90;
+                    } else {
+                        assert false;
+                    }
+                    break;
+                case 'D':
+                    if (newDir == 'L') {
+                        deg += 90;
+                    } else if (newDir == 'R') {
+                        deg -= 90;
+                    } else {
+                        assert false;
+                    }
+                    break;
+                case 'R':
+                    if (newDir == 'U') {
+                        deg -= 90;
+                    } else if (newDir == 'D') {
+                        deg += 90;
+                    } else {
+                        assert false;
+                    }
+                    break;
+                case 'L':
+                    if (newDir == 'U') {
+                        deg += 90;
+                    } else if (newDir == 'D') {
+                        deg -= 90;
+                    } else {
+                        assert false;
+                    }
+                    break;
+                default:
+                    assert false;
+            }
+            dir = newDir;
+        }
+
+        assert deg == 360 || deg == -360;
+        System.out.println("Degrees: " + deg);
+        return deg == 360;
+    }
+
+    // Direction of motion from prev to cur, with the standard Cartesian coordinates (+x to the right, +y upwards)
+    private static char dir(int[] prev, int[] cur) {
+        if (prev[0] == cur[0]) {
+            return prev[1] < cur[1] ? 'U' : 'D';
+        } else {
+            return prev[0] < cur[0] ? 'R' : 'L';
+        }
+    }
+
+    private static boolean hasSizeOneSteps(List<int[]> points) {
+        for (int i = 0; i < points.size(); i++) {
+            if (Math.abs(points.get(i)[0] - points.get((i + 1) % points.size())[0]) == 1
+                || Math.abs(points.get(i)[1] - points.get((i + 1) % points.size())[1]) == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
